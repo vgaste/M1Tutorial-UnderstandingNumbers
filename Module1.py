41341be4f5dcfdebabee4206f294c8cb22e10eb2
vgaste <144867205+vgaste@users.noreply.github.com>
2024-10-27 17:32:25 -0400

    Add files via upload

---
+++ Module1.py
@@ -0,0 +1,35 @@
+#Title: Module1.py
+#Name: Valeria G
+#Date: 10/24/2024
+
+"""
+Revisions: 
+-File created-creator-date
+"""
+
+#1
+pi = 3.14159
+print(pi)
+
+#2
+radius = 5
+area = pi * radius ** 2
+
+#3
+print(area)
+
+#4 
+my_age = 25
+print(my_age)
+
+#5
+age_months = my_age * 12
+print(age_months)
+
+#6
+x = divmod(12345, 789)
+print(x)
+
+x,y = divmod(12345, 789)
+print(x)
+print(y)
